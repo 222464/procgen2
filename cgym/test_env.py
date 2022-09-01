9b047f0900e6051ec88a7f87262b712be37f69ae
Eric Laukien <ericlaukien@gmail.com>
2022-08-31 20:57:48 -0400

    Testing

--- cgym/test_env.py
+++ cgym/test_env.py
@@ -2,7 +2,7 @@ import gym
 import numpy as np
 from cenv import CEnv
 
-env = CEnv("test_env.so")
+env = CEnv("./test_env.so")
 
 for i in range(100):
     obs, reward, term, trunc, info = env.step(0)
